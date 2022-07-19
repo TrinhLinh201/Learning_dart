4885dbe520c757fe84d7e020ac007691ef0b1e01
TrinhLinh201 <97020160+TrinhLinh201@users.noreply.github.com>
2022-07-20 00:25:17 +0700

    Update tim_so_nguyen_to_trong_mang_2_chieu.dart

--- tim_so_nguyen_to_trong_mang_2_chieu.dart
+++ tim_so_nguyen_to_trong_mang_2_chieu.dart
@@ -1,37 +1,41 @@
 import 'dart:io';
+import 'dart:math';
 
 void main() {
-  List<List<int>> arr = [[]];
-  int n = 3, m = 4;
-  nhapMang2Chieu(arr, n, m);
-  inxMang2Chieu(arr, n, m);
+  int row = 3, col = 4;
+  List arr = List.generate(
+      row, (_) => List.generate(col, (_) => int.parse(stdin.readLineSync()!)));
+  print("print matrix:");
+  printPrime(arr, row, col);
+  //printMatrix(arr, row);
 }
 
-void nhapMang2Chieu(List<List<int>> arr, int n, int m) {
-  List<int> arr2 = [];
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      arr2.add(int.parse(stdin.readLineSync()!));
-    }
-    arr.add(arr2);
-    arr2.clear();
+// void printMatrix(List arr, int row) {
+//   for (int i = 0; i < row; i++) {
+//     print("${arr[i]}");
+//   }
+// }
+
+bool isPrime(int n) {
+  if (n < 2) {
+    return false;
   }
+  //int squareRoot = int. sqrt(n);
+  int i;
+  for (i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
-void inxMang2Chieu(List<List<int>> arr, int n, int m) {
-  List<int> arr2 = [];
-  for (int i = 0; i < n; i++) {
-    arr2 = arr[i];
-    for (int j = 0; j < m; j++) {
-      if (arr2[j] < 5) {
-        print("${arr[j]}");
+void printPrime(List arr, int row, int col) {
+  for (var i = 0; i < row; i++) {
+    for (var j = 0; j < col; j++) {
+      if (isPrime(arr[i][j])) {
+        print("${arr[i][j]}");
       }
     }
-
-    arr2.clear();
   }
-}
-
-void inMang2Chieu(List<List<int>> arr) {
-  print("$arr");
 }
