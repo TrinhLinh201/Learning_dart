b423fc7ea55a56ff4c22ec886cc5b2cd02fbc528
TrinhLinh201 <97020160+TrinhLinh201@users.noreply.github.com>
2022-07-19 22:08:29 +0700

    Update tim_so_nguyen_to_trong_mang_2_chieu.dart

--- tim_so_nguyen_to_trong_mang_2_chieu.dart
+++ tim_so_nguyen_to_trong_mang_2_chieu.dart
@@ -4,7 +4,7 @@ void main() {
   List<List<int>> arr = [[]];
   int n = 3, m = 4;
   nhapMang2Chieu(arr, n, m);
-  //inMang2Chieu(arr, n, m);
+  inxMang2Chieu(arr, n, m);
 }
 
 void nhapMang2Chieu(List<List<int>> arr, int n, int m) {
@@ -14,6 +14,20 @@ void nhapMang2Chieu(List<List<int>> arr, int n, int m) {
       arr2.add(int.parse(stdin.readLineSync()!));
     }
     arr.add(arr2);
+    arr2.clear();
+  }
+}
+
+void inxMang2Chieu(List<List<int>> arr, int n, int m) {
+  List<int> arr2 = [];
+  for (int i = 0; i < n; i++) {
+    arr2 = arr[i];
+    for (int j = 0; j < m; j++) {
+      if (arr2[j] < 5) {
+        print("${arr[j]}");
+      }
+    }
+
     arr2.clear();
   }
 }
