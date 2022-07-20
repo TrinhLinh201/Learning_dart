423f2be63c6e0f857319548475b7038555dd7529
TrinhLinh201 <97020160+TrinhLinh201@users.noreply.github.com>
2022-07-20 10:14:38 +0700

    Create noi_2_xau.dart

---
+++ noi_2_xau.dart
@@ -0,0 +1,11 @@
+import 'dart:ffi';
+import 'dart:io';
+
+void main() {
+  String str1, str2;
+  print("input string 1:");
+  str1 = stdin.readLineSync()!;
+  print("input string 2:");
+  str2 = stdin.readLineSync()!;
+  print("string 1 + string 2: ${str1 + str2}");
+}
